40dba38c12fc2c6d853ae5022aa74f0309316596
EntropyRiser <82279347+EntropyRiser@users.noreply.github.com>
2023-04-01 00:56:16 +0800

    Add files via upload
    Add crossfade and pre-pad function.

--- gui.py
+++ gui.py
@@ -1,11 +1,11 @@
 import PySimpleGUI as sg
 import sounddevice as sd
-import torch,librosa,threading,time
+import torch,librosa,threading,time,queue
 from enhancer import Enhancer
 import numpy as np
 from ddsp.vocoder import load_model, F0_Extractor, Volume_Extractor, Units_Encoder
 from ddsp.core import upsample
-
+import matplotlib.pyplot as plt
 def launcher():
     '''窗口加载'''
     sg.theme('DarkAmber')   # 设置主题
@@ -24,65 +24,88 @@ def launcher():
             [sg.Text("变调"),sg_pitch],
             [sg.Text("f0预测模式"),sg_f0mode],
             [sg.Text("采样率"),sg_samplerate]
-            ],title='设置')],
+            ],title='设置'),
+        sg.Frame(layout=[
+            [sg.Text("交叉淡化时长"),sg_crossfade],
+            [sg.Text("使用历史区块数量"),sg_buffer_num]
+            ],title='设置')
+         ],
         [sg.Button("开始音频转换",key="start_vc"),sg.Button("停止音频转换",key="stop_vc")]
     ]
 
     # 创造窗口
     window = sg.Window('DDSP - GUI by INT16', layout)
     event_handler(window=window)
-    
+
+
 def audio_callback(indata,outdata, frames, time, status):
     '''音频处理'''
+    global input_wav,output_wav,temp_wav,crossfade_last
     #print('indata shape:'+str(indata.shape))
     print("Realtime VCing...")
-    input_wav=librosa.to_mono(indata.T)
-    input_wav[:fade_samples]*=fade_in_window
-    input_wav[-fade_samples:]*=fade_out_window
+    input_wav[:]=np.roll(input_wav,-block_frame)
+    input_wav[-block_frame:]=librosa.to_mono(indata.T)
+    print('input_wav.shape:'+str(input_wav.shape))
     _audio, _model_sr = svc_model.infer( f_pitch_change, spk_id, f_safe_prefix_pad_length,input_wav,samplerate)
-    #cut_size=int((_audio.shape[0]*samplerate/_model_sr-outdata.shape[0])/2)
+    temp_wav[:] = librosa.resample(_audio, orig_sr=_model_sr, target_sr=samplerate)[-block_frame-crossfade_frame:]
+    #cross fade output_wav's start with last crossfade
+    output_wav[:]=temp_wav[:block_frame]
+    output_wav[:crossfade_frame]*=fade_in_window
+    output_wav[:crossfade_frame]+=crossfade_last
+    crossfade_last[:]=temp_wav[-crossfade_frame:]
+    crossfade_last[:]*=fade_out_window
+    #output_wav[:]=librosa.resample(_audio, orig_sr=_model_sr, target_sr=samplerate)[-block_frame:]
+    print("infered _audio.shape:"+str(_audio.shape)+',samplerate:'+str(_model_sr))
+    outdata[:] = np.array([output_wav, output_wav]).T
+    print('Outputed.')
+    
+    '''
     output_wave:np.ndarray = librosa.resample(_audio, orig_sr=_model_sr, target_sr=samplerate)[:outdata.shape[0]]
     output_wave[:fade_samples]*=fade_in_window
     output_wave[-fade_samples:]*=fade_out_window
-    outdata[:] = np.array([output_wave, output_wave]).T
-    
-    #debug
     '''
-    time2 = np.arange(0, len(input_wav)) * (1.0 / samplerate)
+    
+    
+    '''time2 = np.arange(0, len(input_wav)) * (1.0 / samplerate)
     plt.cla()
     plt.plot(time2, input_wav)
-    plt.title("语音信号时域波形")
-    plt.xlabel("时长（秒）")
-    plt.ylabel("振幅")
+    plt.title("input_wave")
+    plt.xlabel("time")
+    plt.ylabel("amp")
     plt.savefig(".\\input.png", dpi=600)
-    time2 = np.arange(0, len(_audio)) * (1.0 / _model_sr)
+    time2 = np.arange(0, len(_audio)) * (1.0 / samplerate)
     plt.cla()
     plt.plot(time2, _audio)
-    plt.title("语音信号时域波形")
-    plt.xlabel("时长（秒）")
-    plt.ylabel("振幅")
-    plt.savefig(".\\output.png", dpi=600)
-    '''
+    plt.title("_audio")
+    plt.xlabel("time")
+    plt.ylabel("amp")
+    plt.savefig(".\\output.png", dpi=600)'''
+    
+    
 
 def soundinput():
     '''
     接受音频输入
     '''
     global flag_vc
-    with sd.Stream(callback=audio_callback, blocksize=int(samplerate * block_size),samplerate=samplerate):
+    with sd.Stream(callback=audio_callback, blocksize=block_frame,samplerate=samplerate,dtype='float32'):
         while flag_vc:
-            time.sleep(block_size)
+            time.sleep(block_time)
             print('Audio block passed.')
     print('ENDing VC')
 
 
 def start_vc():
     '''开始音频转换'''
-    global flag_vc,svc_model,fade_in_window,fade_out_window,fade_samples
+    global flag_vc,svc_model,fade_in_window,fade_out_window,input_wav,output_wav,temp_wav,crossfade_last
     flag_vc = True
-    fade_samples=int(samplerate*fade_time)
-    fade_in_window = np.linspace(0, 1,fade_samples )
-    fade_out_window = np.linspace(1, 0, fade_samples)
+    #初始化一下各个ndarray
+    input_wav=np.zeros(int((1+buffer_num)*block_frame),dtype='float32')
+    output_wav=np.zeros(block_frame,dtype='float32')
+    temp_wav=np.zeros(block_frame+crossfade_frame,dtype='float32')
+    crossfade_last=np.zeros(crossfade_frame,dtype='float32')
+    fade_in_window = np.linspace(0, 1,crossfade_frame)
+    fade_out_window = np.linspace(1, 0,crossfade_frame)
     svc_model = SvcDDSP(checkpoint_path, use_vocoder_based_enhancer, enhancer_adaptive_key, select_pitch_extractor,limit_f0_min, limit_f0_max, threhold, spk_id, spk_mix_dict, enable_spk_id_cover)
     thread_vc=threading.Thread(target=soundinput)
     thread_vc.start()
@@ -90,7 +113,7 @@ def start_vc():
 
 def event_handler(window):
     '''事件处理'''
-    global flag_vc,block_size,f_pitch_change,f_safe_prefix_pad_length,threhold,checkpoint_path
+    global flag_vc,block_time,f_pitch_change,f_safe_prefix_pad_length,threhold,checkpoint_path,samplerate,buffer_num,crossfade_time,block_frame,crossfade_frame
     while True:#事件处理循环
         event, values = window.read()
         if event ==sg.WINDOW_CLOSED:   # 如果用户关闭窗口或点击`Cancel`
@@ -101,18 +124,22 @@ def event_handler(window):
             #set values
             checkpoint_path = sg_model.get()
             f_pitch_change = values['pitch']
-            f_safe_prefix_pad_length= 0
+            buffer_num= int(values['buffernum'])
             threhold=values['noise']
-            block_size=values['block']
-            samplerate=values['samplerate']
+            crossfade_time=values['crossfade']
+            block_time=float(values['block'])
+            samplerate=int(values['samplerate'])
+            block_frame=int(block_time*samplerate)
+            crossfade_frame=int(crossfade_time*samplerate)
+            f_safe_prefix_pad_length=block_time*(buffer_num)-crossfade_time*2
+            print('crossfade_time:'+str(crossfade_time))
+            print("buffer_num:"+str(buffer_num))
+            print("samplerate:"+str(samplerate))
+            print('block_time:'+str(block_time))
+            print("prefix_pad_length:"+str(f_safe_prefix_pad_length))
             start_vc()
         if event=='stop_vc'and flag_vc==True:
             flag_vc = False
-
-
-class vc_thread(threading.Thread):
-    def terminate(self):
-        self._running
 
 
 def set_devices(input_device,output_device):
@@ -124,9 +151,7 @@ def set_devices(input_device,output_device):
     sd.default.device[1]=output_device_indices[output_devices.index(output_device)]
     print("input device:"+str(sd.default.device[0])+":"+str(input_device))
     print("output device:"+str(sd.default.device[1])+":"+str(output_device))
-    device_info = sd.query_devices(kind='input')
-    samplerate = int(device_info['default_samplerate'])
-    print("samplerate:"+str(samplerate))
+    
     
 
 def get_devices(update: bool = True):
@@ -190,8 +215,6 @@ class SvcDDSP:
         print("Infering...")
         # load input
         #audio, sample_rate = librosa.load(input_wav, sr=None, mono=True)
-        if len(audio.shape) > 1:
-            audio = librosa.to_mono(audio)
         hop_size = self.args.data.block_size * sample_rate / self.args.data.sampling_rate
         # safe front silence
         if safe_prefix_pad_length > 0.03:
@@ -251,25 +274,34 @@ class SvcDDSP:
 
 
 if __name__ == "__main__":
-    flag_vc=False
+    #全局变量
+    flag_vc=False#变声线程flag
     samplerate=44100#Hz
-    block_size=1.5#s
-    fade_time=0.01#s
-    fade_in_window=None
+    block_time=1.5#s
+    block_frame=0
+    crossfade_frame=0
+    crossfade_time=0.1#s
+    fade_in_window=None#crossfade计算器
     fade_out_window=None
-    fade_samples=None
+    f_safe_prefix_pad_length:float = 1.0
+    input_wav:np.ndarray=None
+    output_wav:np.ndarray=None
+    temp_wav:np.ndarray=None
+    f_pitch_change = 0#float(request_form.get("fPitchChange", 0))
+    crossfade_last:np.ndarray=None#保存上一个output的crossfade
     f0_mode=["parselmouth", "dio", "harvest", "crepe"]
     input_devices,output_devices,input_device_indices, output_device_indices=get_devices()
     sg_model=sg.Input(key='sg_model',default_text='exp\\model_chino.pt')
+    '''gui组件'''
     sg_input_device=sg.Combo(input_devices,key='sg_input_device',default_value=input_devices[sd.default.device[0]])
     sg_output_device=sg.Combo(output_devices,key='sg_output_device',default_value=output_devices[sd.default.device[1]])
     sg_noise=sg.Slider(range=(-60,0),orientation='h',key='noise',resolution=1,default_value=-35)
     sg_block=sg.Slider(range=(0.1,3.0),orientation='h',key='block',resolution=0.05,default_value=0.5)
     sg_pitch=sg.Slider(range=(-24,24),orientation='h',key='pitch',resolution=1,default_value=12)
+    sg_crossfade=sg.Slider(range=(0.02,0.1),orientation='h',key='crossfade',resolution=0.01)
+    sg_buffer_num=sg.Slider(range=(1,10),orientation='h',key='buffernum',resolution=1,default_value=1)
     sg_f0mode=sg.Combo(values=f0_mode,key='f0_mode',default_value=f0_mode[2])
-    sg_samplerate=sg.Input(key='samplerate',default_text='48000')
-    f_pitch_change = None#float(request_form.get("fPitchChange", 0))
-    f_safe_prefix_pad_length =None
+    sg_samplerate=sg.Input(key='samplerate',default_text='44100')
     checkpoint_path=""
     # ddsp-svc下只需传入下列参数。
     # 对接的是串串香火锅大佬https://github.com/zhaohui8969/VST_NetProcess-。建议使用最新版本。
