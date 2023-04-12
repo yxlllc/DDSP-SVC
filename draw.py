2c0b8e663e7be8fee45fd159a8ff87a0c6fbeefd
KakaruHayate <kakaru.hk@hotmail.com>
2023-04-12 21:24:27 +0800

    fix 'draw.py' bug

--- draw.py
+++ draw.py
@@ -31,19 +31,24 @@ def split_data(src_dir, dst_dir, ratio):
     if not os.path.exists(dst_dir):
         os.makedirs(dst_dir)
     
-    # 获取源目录下所有的子目录和文件名（不包括子目录下的内容）
-    subdirs, files = [], []
+    # 获取源目录下所有的子目录和文件名
+    subdirs, files, subfiles = [], [], []
     for item in os.listdir(src_dir):
         item_path = os.path.join(src_dir, item)
         if os.path.isdir(item_path):
             subdirs.append(item)
+            for subitem in os.listdir(item_path):
+                subitem_path = os.path.join(item_path, subitem)
+                if os.path.isfile(subitem_path) and subitem.endswith(".wav"):
+                    subfiles.append(subitem)
         elif os.path.isfile(item_path) and item.endswith(".wav"):
             files.append(item)
 
     # 如果源目录下没有任何wav文件，则报错并退出函数
     if len(files) == 0:
-        print(f"Error: No wav files found in {src_dir}")
-        return
+        if len(subfiles) == 0:
+            print(f"Error: No wav files found in {src_dir}")
+            return
     
     # 计算需要抽取的wav文件数量
     num_files = int(len(files) * ratio)
@@ -52,26 +57,22 @@ def split_data(src_dir, dst_dir, ratio):
     # 随机打乱文件名列表，并取出前num_files个作为抽取结果
     np.random.shuffle(files)
     selected_files = files[:num_files]
-
+    
     # 创建一个进度条对象，用于显示程序的运行进度
     pbar = tqdm.tqdm(total=num_files)
 
-    # 遍历抽取结果中的每个文件名
+    # 遍历抽取结果中的每个文件名，检查是否大于2秒
     for file in selected_files:
-        # 拼接源文件和目标文件的完整路径
         src_file = os.path.join(src_dir, file)
-        dst_file = os.path.join(dst_dir, file)
-        # 检查源文件的时长是否大于2秒
-        if check_duration(src_file):
-            # 如果是，则剪切源文件到目标目录中
-            shutil.move(src_file, dst_file)
-            # 更新进度条
-            pbar.update(1)
-        else:
-            # 如果不是，则打印源文件的文件名，并跳过该文件
+        # 检查源文件的时长是否大于2秒，如果不是，则打印源文件的文件名，并跳过该文件
+        if not check_duration(src_file):
             print(f"Skipped {src_file} because its duration is less than 2 seconds.")
-    
-    # 关闭进度条
+            continue
+        # 拼接源文件和目标文件的完整路径，移动文件，并更新进度条
+        dst_file = os.path.join(dst_dir, file)
+        shutil.move(src_file, dst_file)
+        pbar.update(1)
+
     pbar.close()
 
     # 遍历源目录下所有的子目录（如果有）
