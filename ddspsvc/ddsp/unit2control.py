60a4af8801b69d444fc0d07111275a29a98f2bfb
HuanLinOTO <linsmc@126.com>
2024-07-14 20:53:33 +0800

    feat: 多线程 val

--- ddspsvc/ddsp/unit2control.py
+++ ddspsvc/ddsp/unit2control.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import torch
 import torch.nn as nn
@@ -6,7 +5,6 @@ from torch.nn.utils import weight_norm
 
 from .pcmer import PCmer
 from ddspsvc.diffusion.model_conformer_naive import ConformerNaiveEncoder
-
 
 
 def split_to_dict(tensor, tensor_splits):
@@ -24,13 +22,14 @@ def split_to_dict(tensor, tensor_splits):
 
 class Unit2Control(nn.Module):
     def __init__(
-            self,
-            input_channel,
-            n_spk,
-            output_splits,
-            use_pitch_aug=False,
-            pcmer_norm=False,
-            use_naive_v2=False):
+        self,
+        input_channel,
+        n_spk,
+        output_splits,
+        use_pitch_aug=False,
+        pcmer_norm=False,
+        use_naive_v2=False,
+    ):
         super().__init__()
         self.output_splits = output_splits
         self.f0_embed = nn.Linear(1, 256)
@@ -43,13 +42,14 @@ class Unit2Control(nn.Module):
             self.aug_shift_embed = nn.Linear(1, 256, bias=False)
         else:
             self.aug_shift_embed = None
-            
+
         # conv in stack
         self.stack = nn.Sequential(
-                nn.Conv1d(input_channel, 256, 3, 1, 1),
-                nn.GroupNorm(4, 256),
-                nn.LeakyReLU(),
-                nn.Conv1d(256, 256, 3, 1, 1)) 
+            nn.Conv1d(input_channel, 256, 3, 1, 1),
+            nn.GroupNorm(4, 256),
+            nn.LeakyReLU(),
+            nn.Conv1d(256, 256, 3, 1, 1),
+        )
 
         # transformer
         if use_naive_v2:
@@ -60,7 +60,8 @@ class Unit2Control(nn.Module):
                 use_norm=False,
                 conv_only=True,
                 conv_dropout=0,
-                atten_dropout=0.1)
+                atten_dropout=0.1,
+            )
         else:
             self.decoder = PCmer(
                 num_layers=3,
@@ -70,25 +71,31 @@ class Unit2Control(nn.Module):
                 dim_values=256,
                 residual_dropout=0.1,
                 attention_dropout=0.1,
-                pcmer_norm=pcmer_norm)
+                pcmer_norm=pcmer_norm,
+            )
         self.norm = nn.LayerNorm(256)
 
         # out
         self.n_out = sum([v for k, v in output_splits.items()])
-        self.dense_out = weight_norm(
-            nn.Linear(256, self.n_out))
+        self.dense_out = weight_norm(nn.Linear(256, self.n_out))
 
-    def forward(self, units, f0, phase, volume, spk_id = None, spk_mix_dict = None, aug_shift = None):
-        
-        '''
-        input: 
+    def forward(
+        self, units, f0, phase, volume, spk_id=None, spk_mix_dict=None, aug_shift=None
+    ):
+        """
+        input:
             B x n_frames x n_unit
-        return: 
+        return:
             dict of B x n_frames x feat
-        '''
+        """
 
-        x = self.stack(units.transpose(1,2)).transpose(1,2)
-        x = x + self.f0_embed((1+ f0 / 700).log()) + self.phase_embed(phase / np.pi) + self.volume_embed(volume)
+        x = self.stack(units.transpose(1, 2)).transpose(1, 2)
+        x = (
+            x
+            + self.f0_embed((1 + f0 / 700).log())
+            + self.phase_embed(phase / np.pi)
+            + self.volume_embed(volume)
+        )
         if self.n_spk is not None and self.n_spk > 1:
             if spk_mix_dict is not None:
                 for k, v in spk_mix_dict.items():
@@ -102,6 +109,5 @@ class Unit2Control(nn.Module):
         x = self.norm(x)
         e = self.dense_out(x)
         controls = split_to_dict(e, self.output_splits)
-    
-        return controls, x
 
+        return controls, x
--- ddspsvc/logger/__init__.py
+++ ddspsvc/logger/__init__.py
@@ -4,46 +4,13 @@ from rich.console import Console
 from rich.logging import RichHandler
 from rich.progress import Progress as _Progress
 from rich.progress import (
-    Progress,
     TextColumn,
     BarColumn,
     TimeElapsedColumn,
     TimeRemainingColumn,
 )
 
-import time
-
-import os
-import datetime
-
 console = Console(stderr=None)
-
-logger.remove()
-
-
-def format_level(str, length):
-    if len(str) < length:
-        str = str + " " * (length - len(str))
-    else:
-        str = str
-    # 给 str 上对应 level 的颜色
-    if str == "INFO   ":
-        str = f"[bold green]{str}[/bold green]"
-    elif str == "WARNING":
-        str = f"[bold yellow]{str}[/bold yellow]"
-    elif str == "ERROR  ":
-        str = f"[bold red]{str}[/bold red]"
-    elif str == "DEBUG  ":
-        str = f"[bold cyan]{str}[/bold cyan]"
-    return str
-
-def default_format(record):
-    return f"[green]{record['time'].strftime('%Y-%m-%d %H:%M:%S')}[/green] | [level]{format_level(record['level'].name,7)}[/level] | - [level]{record['message']}[/level]\n"
-
-
-logger.add(lambda m: console.print(m, end=""), format=default_format, colorize=True)
-
-
 def addLogger(path):
     logger.add(
         path,
