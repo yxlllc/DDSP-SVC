0e7a92d6f2ffd28ac3c714541908927686a1de4f
yxlllc <llc1995@sina.com>
2025-11-16 15:36:25 +0800

    optimize

--- optimizer/muon.py
+++ optimizer/muon.py
@@ -4,7 +4,22 @@ import torch.nn.functional as F
 from torch import Tensor
 from torch.nn import Module, Parameter, Embedding
 from typing import List
+from itertools import repeat
 from .chained_optimizer import ChainedOptimizer, OptimizerSpec
+
+coeffs_list = [
+    (8.28721201814563, -23.595886519098837, 17.300387312530933),
+    (4.107059111542203, -2.9478499167379106, 0.5448431082926601),
+    (3.9486908534822946, -2.908902115962949, 0.5518191394370137),
+    (3.3184196573706015, -2.488488024314874, 0.51004894012372),
+    (2.300652019954817, -1.6689039845747493, 0.4188073119525673),
+    (1.891301407787398, -1.2679958271945868, 0.37680408948524835),
+    (1.8750014808534479, -1.2500016453999487, 0.3750001645474248),
+    (1.875, -1.25, 0.375), # subsequent coeffs equal this numerically
+]
+
+# safety factor for numerical stability (but exclude last polynomial )
+coeffs_list = [(a / 1.01 , b / 1.01**3 , c / 1.01**5) for (a, b, c) in coeffs_list[: -1]] + [coeffs_list[-1]]
 
 
 def get_bf16_support_map():
@@ -24,7 +39,7 @@ def get_bf16_support_map():
         
     return bf16_support_map
     
-    
+
 def zeropower_via_newtonschulz5(G: Tensor, steps: int, use_bf16: bool) -> Tensor:
     """
     Newton-Schulz iteration to compute the zeroth power / orthogonalization of G. We opt to use a
@@ -36,26 +51,49 @@ def zeropower_via_newtonschulz5(G: Tensor, steps: int, use_bf16: bool) -> Tensor
     performance at all relative to UV^T, where USV^T = G is the SVD.
     """
     assert G.ndim == 3 # batched Muon implementation by @scottjmaddox, and put into practice in the record by @YouJiacheng
-    a, b, c = (3.4445, -4.7750,  2.0315)
-    if use_bf16:
-        X = G.bfloat16()
-    else:
-        X = G.float()
-    if G.size(-2) > G.size(-1):
-        X = X.mT
+    #a, b, c = (3.4445, -4.7750,  2.0315)
+    
+    X = G.to(dtype = torch.bfloat16 if use_bf16 else torch.float32)
 
     # Ensure spectral norm is at most 1
     X = F.normalize(X, p=2.0, dim=(-2, -1), eps=1e-7)
     
     # Perform the NS iterations
-    for _ in range(steps):
-        A = X @ X.mT
-        B = torch.baddbmm(A, A, A, beta=b, alpha=c)
-        X = torch.baddbmm(X, B, X, beta=a, alpha=1)
-    
-    if G.size(-2) > G.size(-1):
-        X = X.mT
-    return X.to(G)
+    hs = coeffs_list[: steps] + list(repeat(coeffs_list[-1], steps - len(coeffs_list)))
+    if use_bf16:
+        if X.size(-2) < X.size(-1):
+            for a, b, c in hs:
+                A = torch.bmm(X, X.mT)
+                A = torch.baddbmm(A, A, A, beta=b, alpha=c)
+                X = torch.baddbmm(X, A, X, beta=a, alpha=1)
+        else:
+            for a, b, c in hs:
+                A = torch.bmm(X.mT, X)
+                A = torch.baddbmm(A, A, A, beta=b, alpha=c)
+                X = torch.baddbmm(X, X, A, beta=a, alpha=1)
+    else:
+        if X.size(-2) < X.size(-1):
+            for a, b, c in hs:
+                d1 = b / 2 / c
+                d0 = a - b * b / 4 / c
+                A = torch.bmm(X, X.mT)
+                A.diagonal(dim1=-2, dim2=-1).add_(d1)
+                A = torch.bmm(A, A)
+                A.mul_(c)
+                A.diagonal(dim1=-2, dim2=-1).add_(d0)
+                X = torch.bmm(A, X)
+        else:
+            for a, b, c in hs:
+                d1 = b / 2 / c
+                d0 = a - b * b / 4 / c
+                A = torch.bmm(X.mT, X)
+                A.diagonal(dim1=-2, dim2=-1).add_(d1)
+                A = torch.bmm(A, A)
+                A.mul_(c)
+                A.diagonal(dim1=-2, dim2=-1).add_(d0)
+                X = torch.bmm(X, A)
+            
+    return X
 
 
 class Muon(torch.optim.Optimizer):
@@ -85,7 +123,7 @@ class Muon(torch.optim.Optimizer):
         defaults = dict(lr=lr, weight_decay=weight_decay, momentum=momentum, nesterov=nesterov, ns_steps=ns_steps)
         super().__init__(params, defaults)
         self.bf16_support_map = get_bf16_support_map()
-        
+    
     @torch.no_grad()
     def step(self, closure=None):
         for group in self.param_groups:
@@ -95,30 +133,31 @@ class Muon(torch.optim.Optimizer):
                 state = self.state[p]
                 if "momentum_buffer" not in state:
                     state["momentum_buffer"] = torch.zeros_like(g)
-                buf: Tensor = state["momentum_buffer"]
                 key = (p.shape, p.device, p.dtype)
                 if key not in shape_groups:
                     shape_groups[key] = {"params": [], "grads": [], "buffers": []}
                 shape_groups[key]["params"].append(p)
                 shape_groups[key]["grads"].append(g)
-                shape_groups[key]["buffers"].append(buf)
+                shape_groups[key]["buffers"].append(state["momentum_buffer"])
             for key in shape_groups:
                 group_data = shape_groups[key]
-                g = torch.stack(group_data["grads"])
-                buf = torch.stack(group_data["buffers"])
-                buf.lerp_(g, 1 - group["momentum"])
-                g = g.lerp_(buf, group["momentum"]) if group["nesterov"] else buf
+                p, g, buf, m = group_data["params"], group_data["grads"], group_data["buffers"], group["momentum"]
+                torch._foreach_lerp_(buf, g, 1-m)
+                if group["nesterov"]:
+                    torch._foreach_lerp_(g, buf, m)
+                    g = torch.stack(g)
+                else:
+                    g = torch.stack(buf)
+                original_shape = g.shape
                 if g.ndim >= 4:  # for the case of conv filters
                     g = g.view(g.size(0), g.size(1), -1)
                 use_bf16 = self.bf16_support_map.get(g.device, False)
                 g = zeropower_via_newtonschulz5(g, steps=group["ns_steps"], use_bf16=use_bf16)
-                for i, p in enumerate(group_data["params"]):
-                    if group["weight_decay"] > 0:
-                        p.data.mul_(1 - group["lr"] * group["weight_decay"])
-                    p.data.add_(g[i].view_as(p), alpha=-group["lr"] * max(g[i].size()) ** 0.5)
-                    self.state[p]["momentum_buffer"] = buf[i].clone()
-
-
+                if group["weight_decay"] > 0:
+                    torch._foreach_mul_(p, 1 - group["lr"] * group["weight_decay"])
+                torch._foreach_add_(p, g.view(original_shape).unbind(0), alpha=-group["lr"] * max(g[0].size()) ** 0.5)
+                
+                
 def get_params_for_muon(model) -> List[Parameter]:
     """
     Filter parameters of a module into two groups: those that can be optimized by Muon,
